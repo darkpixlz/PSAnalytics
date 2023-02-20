a12bbe0aa91051908d7d28a98b38b03a731f9517
Pixlz <91509874+darkpixlz@users.noreply.github.com>
2023-02-20 02:48:13 -0500

    push unworking code

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import threading
 app = Flask("PS_Analytics")
 placeids = []
 
+
 @app.route("/somethingelse/<ServerID>", methods=["POST"])
 def Server(ServerID):
     try:
@@ -16,13 +17,67 @@ def Server(ServerID):
     except:
         return abort(400)
 
+
+@app.route('/error-logs', methods=['POST'])
+def error_logs():
+    error_data = json.loads(request.data.decode('utf-8'))
+    error_contents = error_data.get('ErrorContents', '')
+    place_id = error_data.get('PlaceId', '')
+    log_entry = f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} / {place_id}]: {error_contents}\n"
+
+    with open('files/error_logs.txt', 'a') as f:
+        f.write(log_entry)
+
+    return 'Logged error successfully.'
+
+@app.route('/asset-loaded', methods=['POST'])
+def assets_loaded():
+    data = json.loads(request.data)
+    assets_loaded = data['AssetsLoaded']
+    place_id = str(data['PlaceId'])
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    with open('files/AssetsLoaded.json', 'r+') as f:
+        try:
+            file_data = json.load(f)
+        except json.JSONDecodeError:
+            file_data = {}
+
+        # Check if the place_id already exists in the data
+        if place_id in file_data:
+            # Update the existing entry with the new assets_loaded value
+            file_data[place_id]['AssetsLoaded'] += assets_loaded
+        else:
+            # Create a new entry for the place_id
+            file_data[place_id] = {'AssetsLoaded': assets_loaded, 'PlaceId': place_id}
+
+        # Check if it's a new day
+        if not file_data or datetime.datetime.strptime(list(file_data.keys())[-1], '%Y-%m-%d %H:%M:%S').date() != datetime.datetime.now().date():
+            # If it's a new day, add a new entry to the file
+            file_data[timestamp] = {place_id: file_data[place_id]}
+        else:
+            # If it's the same day, update the last entry in the file
+            last_key = list(file_data.keys())[-1]
+            file_data[last_key][place_id] = {**file_data[last_key][place_id], 'AssetsLoaded': file_data[place_id]['AssetsLoaded']}
+
+        # Write the updated data back to the file
+        f.seek(0)
+        json.dump(file_data, f, indent=2)
+
+    return 'Assets loaded successfully.'
+
+
+
 def Rotate():
     todays_servers = len(placeids)
-    analytics = [obj for obj in json.loads(open("files/analytics.json", "r").read())]
-    analytics.append(f"{[datetime.datetime.now().split(' ')[0]]} = {todays_servers}")
+    analytics = [obj for obj in json.loads(
+        open("files/analytics.json", "r").read())]
+    analytics.append(
+        f"{[datetime.datetime.now().split(' ')[0]]} = {todays_servers}")
     with open("analytics.json", "a") as handle:
         handle.write(json.dumps(analytics))
-    
+
+
 def RotateScheduler():
     while True:
         CurrentTime = datetime.datetime.now().time()
@@ -33,7 +88,9 @@ def RotateScheduler():
             print("Rotating")
             thread.join()
 
+
 CorrectKey = ""
+
 
 @app.route("/analytics/download/<PrivateKey>", methods=["GET"])
 def download(PrivateKey):
@@ -41,6 +98,7 @@ def download(PrivateKey):
         return open("analytics.json").read()
     else:
         abort(401)
+
 
 app.run(
     port=80,
